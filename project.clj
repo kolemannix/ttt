e37aabc0669f6808013feeb197c6e1129bfa6588
Koleman Nix <kolemannix@gmail.com>
2014-06-30 11:18:05 -0500

    description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject ttt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Plays tictactoe using simple minimax algorithm"
   :url "http://github.com/kolemannix/ttt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
