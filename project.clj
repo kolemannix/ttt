b7c44061c50d9f4da924c31c80894395b28c66f2
Koleman Nix <kolemannix@gmail.com>
2014-06-30 11:16:51 -0500

    remove expectation plugin

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
 (defproject ttt "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/kolemannix/ttt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-autoexpect "1.0"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [midje "1.6.3"]]
-  :main ttt.console
-  :profiles {:uberjar {:aot :all}})
+  :main ttt.console)
